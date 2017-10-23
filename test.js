ae0545cf8f1d8502c2eb26046a7edf808f67ffc6
kazar4 <kazar444@icloud.com>
2017-10-23 14:58:59 -0400

    Main Build

---
+++ test.js
@@ -0,0 +1,56 @@
+
+
+var posX = 0;
+var posY = 0;
+
+//our box element
+var box = document.getElementById("block");
+var holder = document.getElementsByClassName("e");
+
+//var t = setInterval(move, 100);
+
+function move() {
+  box.style.right = posX + "px";
+  box.style.top = posY + "px";
+  posX += 3;
+  posY += 10;
+  }
+
+/*
+var p = document.createElement("p");
+ var node = document.createTextNode("Some new text");
+ //adding the text to the paragraph
+ p.appendChild(node);
+
+//setInterval(write, 5000);
+
+// starting position
+var pos = 0;
+var r = 0;
+//our box element
+var box = document.getElementById("block");
+
+var t = setInterval(move, 10);
+var z = setInterval(raidus, 250);
+
+function raidus() {
+
+  r += 5
+
+  box.style.borderRadius = r + "%";
+}
+
+function move() {
+  if(pos >= 300) {
+    clearInterval(t);
+  }
+  else {
+    pos += 1;
+    box.style.left = pos+"px";
+  }
+}
+
+function write() {
+  document.writeln("Hi");
+}
+*/
