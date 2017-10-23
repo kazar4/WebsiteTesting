434c09e9a6689b9878bed16e0f5b111db4cdfde7
kazar4 <kazar444@icloud.com>
2017-10-23 15:09:53 -0400

    Making a simpler design

--- test.js
+++ test.js
@@ -3,7 +3,6 @@
 var posX = 0;
 var posY = 0;
 
-//our box element
 var box = document.getElementById("block");
 var holder = document.getElementsByClassName("e");
 
@@ -14,43 +13,4 @@ function move() {
   box.style.top = posY + "px";
   posX += 3;
   posY += 10;
-  }
-
-/*
-var p = document.createElement("p");
- var node = document.createTextNode("Some new text");
- //adding the text to the paragraph
- p.appendChild(node);
-
-//setInterval(write, 5000);
-
-// starting position
-var pos = 0;
-var r = 0;
-//our box element
-var box = document.getElementById("block");
-
-var t = setInterval(move, 10);
-var z = setInterval(raidus, 250);
-
-function raidus() {
-
-  r += 5
-
-  box.style.borderRadius = r + "%";
 }
-
-function move() {
-  if(pos >= 300) {
-    clearInterval(t);
-  }
-  else {
-    pos += 1;
-    box.style.left = pos+"px";
-  }
-}
-
-function write() {
-  document.writeln("Hi");
-}
-*/
