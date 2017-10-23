38269aee9d9936e58c1e57f437ebde68670a6c4e
kazar4 <kazar444@icloud.com>
2017-10-23 15:50:05 -0400

    MIGHT NOT USE

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
-
+var box = document.getElementById("block");
+//var holder = document.getElementsByClassName("box e");
+//var e =  holder[0];
 
 var posX = 0;
 var posY = 0;
 
-var box = document.getElementById("block");
-var holder = document.getElementsByClassName("box e");
 
-//var t = setInterval(move, 100);
+var t = setInterval(move, 100);
 
 function move() {
-  box.style.right = posX + "px";
-  box.style.top = posY + "px";
-  posX += 3;
-  posY += 10;
+  if (posY > 300){
+    posX = posX + 3;
+    posY = posY + 10;
+    box.style.right = posX + "px";
+    box.style.top = posY + "px";
+ } else {
+
+  }
 }
